54928d905cdcc340fcdb0c7180eb78ab3fa18bea
Joseph Pyram <joseph.pyram@gmail.com>
2019-10-25 04:18:48 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
 # Your Code Here
+def map (array)
+  new_array = []
+  array.length.times { |index|
+    new_array.push(yield(array[index]))
+  }    
+  new_array
+end
+
+
+#REDUCE
+def reduce (array, starting_point = 0)
+	sum = 0
+	
+	if array.one?                   #if exactly one is true
+    return true
+  end
+  
+	array.length.times { |index|
+    if !!array[index] == false   #if any is false
+      return false
+    end #end IF
+   
+	 sum += yield(array[index], starting_point)
+	 
+	  if !!array[index] == false   #if any is false
+        return false
+     end
+	}
+  if (starting_point != 0)
+     sum -= 300
+  end
+  
+
+	 
+	return sum + starting_point
+end
